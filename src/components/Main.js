eeb6b07c2de40801eefaebb0c82dc389312e6a6b
Shola <sholaemmanuel00@gmail.com>
2018-10-31 17:03:56 +0100

    medium Todo app

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,33 +1,158 @@
+'use strict';
 import React from 'react'
-import { Platform, Image, Text, View } from 'react-native'
+import {
+  AppRegistry,
+  ActivityIndicator,
+  AsyncStorage,
+  ListView,
+  Text,
+  View,
+  TextInput,
+} from 'react-native';
+import { Header,Container,Title, Content, Icon,  Card, CardItem ,Button, Fab,Footer } from 'native-base';
 import firebase from 'firebase';
+//Pages and styles
+import Login from './Login';
+import ListItem from '../components/ListItem.js';
+import styles from './mainstyle.js';
 
 export default class Main extends React.Component {
-  state = {
-    currentUser: null
+  constructor(props) {
+    super(props);
+    this.tasksRef = this.props.firebaseApp.database().ref();
+    this.state = {
+      user: null,
+      loading: true,
+      newTask: ""
+    }
+  }
+
+  componentDidMount() {
+    // start listening for firebase updates
+    this.listenForTasks(this.tasksRef);
   }
 
   componentWillMount() {
-    const { currentUser } = firebase.auth()
-    this.setState({ currentUser })
+    // get the current user from firebase
+    // const userData = this.props.firebaseApp.auth().currentUser;
+    AsyncStorage.getItem('userData').then((user_data_json) => {
+      let userData = JSON.parse(user_data_json);
+      this.setState({
+        user: userData,
+        loading: false,
+        active:'true',
+        tasks:[]
+      });
+    });
   }
 
-  render() {
-    const { currentUser } = this.state
+  //listener to get data from firebase and update listview accordingly
+  listenForTasks() {
+    tasksRef.on('value', (dataSnapshot) => {
+      var tasks = [];
+      dataSnapshot.forEach((child) => {
+        tasks.push({
+          name: child.val().name,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        tasks:tasks
+      })
+    });
+  }
+
+  //add a new task to firebase app
+  _addTask() {
+    // console.log("task value",this.state.newTask);
+    if (this.state.newTask === "") {
+     return;
+    }
+    this.tasksRef.push({ name: this.state.newTask});
+    this.setState({newTask: ""});
+    alert("Task added successfully");
+  }
+
+  _renderItem(task) {
+    // console.log("task",task._key);
+    const onTaskCompletion= () => {
+      // console.log("clickrecived",this.tasksRef.child(task._key).remove());
+      this.tasksRef.child(task._key).remove().then(
+        function() {
+          // fulfillment
+          alert("The task "+task.name+" has been completed successfully");
+        },
+        function() {
+          // fulfillment
+          alert("The task "+task.name+" has not been removed successfully");
+        });
+    }
     return(
-      <View style={styles.container}>
-        <Text>
-          Hi {currentUser && currentUser.email}!
-        </Text>
-     </View>
+      <ListItem task={task} onTaskCompletion={onTaskCompletion} />
+    );
+  }
+
+  logout() {
+    // logout, once that is complete, return the user to the login screen.
+      AsyncStorage.removeItem('userData').then(() => {
+        this.props.firebaseApp.auth().signOut().then(() => {
+          this.props.navigator.push({
+            component: Login
+          });
+        });
+      });
+    }
+
+  render() {
+    // console.log("tasks value",this.state.tasks);
+    // If we are loading then we display the indicator, if the account is null and we are not loading
+    // Then we display nothing. If the account is not null then we display the account info.
+    const content = this.state.loading ?
+    <ActivityIndicator size="large"/> :
+      this.state.user &&
+        <Content>
+          <Card dataArray={this.state.tasks}
+            renderRow={(task) => this._renderItem(task)} >
+          </Card>
+        </Content>
+      ;
+       // console.log("loading user",this.state.user,this.state.loading);
+    return(
+      <Container>
+        <Header>
+          <Button transparent>
+            <Icon name='ios-menu' />
+          </Button>
+          <Title>To Do</Title>
+          <Button transparent onPress={() => this.logout()}>
+                <Icon name='ios-arrow-back' />
+          </Button>
+        </Header>
+
+        {content}
+        <Footer style={styles.footer}>
+          <TextInput
+            value={this.state.newTask}
+            style={styles.textEdit}
+            onChangeText={(text) => this.setState({newTask: text})}
+            placeholder="New Task"
+          />
+            <Fab
+               active={this.state.active}
+               containerStyle={{ marginRight: 0,width:20 }}
+               style={styles.floatButton}
+               position="bottomRight"
+               onPress={() => this._addTask()}
+            >
+              <Icon name="md-add" />
+            </Fab>
+
+        </Footer>
+      </Container>
     );
   }
 }
 
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-}
+
+AppRegistry.registerComponent('Account', () => Account);
---
+++ src/components/mainstyle.js
@@ -0,0 +1,19 @@
+export const styles = {
+  textEdit: {
+    bottom: 0,
+    flex: 1,
+    width: 300,
+  },
+  floatButton: {
+    flex: 1,
+    top: 48,
+    left: 50,
+    alignSelf: 'center',
+    backgroundColor: '#34A34F'
+  },
+  footer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  }
+}
